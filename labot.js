fceb38e15861853a187cb1287b494072266273cd
Sebastian Vollnhals <sebastian@vollnhals.info>
2013-03-17 15:21:51 +0100

    bot code

---
+++ labot.js
@@ -0,0 +1,120 @@
+#!/usr/bin/env node
+/**
+	labot.js – an xmpp bot
+**/
+
+var fs = require('fs');
+var path = require('path');
+var xmpp = require('node-xmpp');
+var xml2js = require('xml2js');
+var config = require(path.resolve(__dirname, "config.js"));
+var parser = new xml2js.Parser();
+
+var chat = new xmpp.Client({
+	jid: config.username, 
+	password: config.password
+});
+
+var saidLa = false;
+
+chat.on('online', function() {
+	console.log('online');
+	// c('status').t('zombi online')
+	chat.send(
+		new xmpp.Element('presence', {
+			type: 'available'
+		}).c('show').t('chat').up().c('status').t('ohai')
+	);
+	chat.send(
+		new xmpp.Element('presence', {
+			to: "" + config.room + "@" + config.conference + "/" + config.nickname
+		}).c('x', {
+			xmlns: 'http://jabber.org/protocol/muc'
+		})
+	);
+});  
+
+chat.on('stanza', function(stanza) {
+	if (stanza.is('message') && stanza.attrs.type === 'groupchat' && !fromMe(stanza)) {
+		stanza.children.forEach(function(_item){
+			if (_item.name === 'body') {
+				var _txt = _item.children[0];
+				if (_txt.match(/penis/i)) {
+					sendmsg("Gnihihihi");
+				}
+				if (_txt.match(/vulva/i)) {
+					sendmsg("Gnihihihi");
+				}
+				if (_txt.match(/glitch/i)) {
+					sendmsg(":(");
+				}
+				if (_txt.match(/hach/i)) {
+					sendmsg("<3");
+				}
+				if (_txt.match(/yetzt/i)) {
+					sendmsg("das petz ich.");
+				}
+				if (_txt.match(/fresse/i)) {
+					sendmsg("bin ja schon ruhig.");
+				}
+				if (_txt.match(/berlin/i)) {
+					sendmsg("druff jeschissen.");
+				}
+				if (_txt.match(/purple/i)) {
+					var _id = (parseInt((Math.random() * 1000), 10) % config.meeble.length);
+					sendmsg(config.meeble[_id]);
+				}
+				if (_txt.match(/dingshirn/i)) {
+					var _id = (parseInt((Math.random() * 1000), 10) % config.phrases.length);
+					sendmsg(config.phrases[_id]);
+				}
+			}
+		});
+	} else if (stanza.is('presence')) {
+		if (fromMe(stanza)) {
+			if (saidLa) return;
+			// say LA!
+			sendmsg("LA!")
+			saidLa = true;
+			//setTimeout(start, parseInt(Math.random()*30000,10));
+			//setTimeout(meeble, parseInt(Math.random()*300000,10));
+		} else {
+			// say hello!
+			var _letter = stanza.attrs.from.split(/\//).pop().substr(0,1);
+			var _msg = (stanza.attrs.type !== 'unavailable') ? "\\"+_letter+"/" : "/"+_letter+"\\";
+			sendmsg(_msg);
+		}
+	}
+});
+
+chat.on('error', function(e) {
+	console.log(e);
+});
+
+var start = function() {
+	var _id = (parseInt((Math.random() * 1000), 10) % config.phrases.length);
+	sendmsg(config.phrases[_id]);
+	var _sec = parseInt(Math.random()*3000000,10);
+	console.log(_sec, "bis frase");
+	setTimeout(start, _sec);
+}
+
+var meeble = function() {
+	var _id = (parseInt((Math.random() * 1000), 10) % config.meeble.length);
+	sendmsg(config.meeble[_id]);
+	var _sec = parseInt(Math.random()*3000000,10);
+	console.log(_sec, "bis frase");
+	setTimeout(meeble, _sec);
+}
+
+/* helpers */
+var fromMe = function(stanza) {
+	return (stanza.attrs.from === (config.room+"@"+config.conference+"/"+config.nickname) || stanza.attrs.from.split(/\//).shift() === config.username);
+};
+
+var sendmsg = function(msg) {
+	chat.send(new xmpp.Element('message',{
+		to: config.room+"@"+config.conference,
+		type: 'groupchat'
+	}).c('body').t(msg));
+}
